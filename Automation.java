60152a88dec5b75fe39a9878ff2a8b3045c3d0c1
Sivagurumoorthy <96658298+Sivagurumoorthy@users.noreply.github.com>
2022-01-21 22:24:12 +0530

    Add files via upload

---
+++ Automation.java
@@ -0,0 +1,32 @@
+package week3.day2;
+
+public class Automation {
+
+	public static void main(String[] args) {
+		
+		Automation Robot =new Automation();
+		
+		Robot.Java();
+		Robot.Selenium();
+		Robot.Ruby();
+}
+	
+public void Ruby() {
+		System.out.println("Ruby");
+		
+}
+
+public void Selenium() {
+		System.out.println("Selenium");
+		
+}
+
+public void Java() {
+		System.out.println("Java");
+		
+}
+	
+
+	}
+
+
